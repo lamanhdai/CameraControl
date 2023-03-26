ea25ce0ffa84613c42aada2e374bd0bcd0e23f8d
bruce.lam <bruce.lam@websparks.sg>
2023-03-27 00:45:59 +0700

    init project camera scan QR code

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { Camera, CameraType } from 'expo-camera';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function App() {
+  const [openedCamera, setOpenedCamera] = useState(false);
+
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  const openCamera = () => {
+    setOpenedCamera(true);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {openedCamera&&
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+          barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
+        />
+      }
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+      <View style={styles.button}><Button title={'Tap to open camera'} onPress={openCamera}/></View>
     </View>
   );
 }
@@ -14,7 +52,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  camera: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  cameraContainer: {
+    width: 200,
+    height: 200,
+  },
+  text: {
+    fontSize: 24,
+    color: '#fff',
+    textAlign: 'center'
+  },
+  button: {
+    marginTop: 50,
+  }
 });
